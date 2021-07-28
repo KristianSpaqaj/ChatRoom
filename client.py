f81811a2b7c8d48e49df10ee400f225e1fdf30e3
unknown <you@example.com>
2021-07-28 20:04:11 +0200

    first commit

---
+++ client.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+ip = '127.0.0.1'
+port = 7001
+buffer = 1024
+nickname = input("Choose your nickname: ")
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((ip,port))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(buffer).decode('utf-8')
+            if message == 'Name':
+                client.send(nickname.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print("An error occured!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('utf-8'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
+#dsasdasasdaa
---
+++ server.py
@@ -0,0 +1,57 @@
+import socket
+import threading
+
+ip = '127.0.0.1'
+port = 7001
+buffer = 1024
+
+# server start
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((ip,port))
+s.listen()
+
+# lists of clients and their nicknames
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(buffer)
+            broadcast(message)
+        except:
+            # removes a client from the chat
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left!'.encode('utf-8'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        # accepts connection
+        client, addr = s.accept()
+        print(f"Connected with {str(addr)}")
+
+        # Saves nicknames
+        client.send('Name'.encode('utf-8'))
+        nickname = client.recv(buffer).decode('utf-8')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        # prints nickname
+        print(f"Nickname is {nickname}")
+        broadcast(f"{nickname} joined!\n".encode('utf-8'))
+        client.send('Connected to server!'.encode('utf-8'))
+
+        # start handling thread for client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+receive()
